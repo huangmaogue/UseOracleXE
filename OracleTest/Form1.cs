a8543da2518a8fded6738ce91fabe5dc9d5fc554
huangmaogue <huangmaogue@gmail.com>
2018-01-12 09:52:47 +0800

    test access linked to oraclexe

--- OracleTest/Form1.cs
+++ OracleTest/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DAO = Microsoft.Office.Interop.Access.Dao;
 
 namespace OracleTest
 {
@@ -21,6 +22,18 @@ namespace OracleTest
         {
             MsAccessDAO msa = new MsAccessDAO(@"z:\blank.accdb");
             msa.NonQuery("update Article set JSON=''");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = "z:\\blank.accdb";
+            DAO.DBEngine dbe = new DAO.DBEngine();
+            DAO.Database db = dbe.OpenDatabase(path);
+            DAO.TableDef tbdef = db.CreateTableDef();
+            tbdef.Name = "linkedtable2";//Whatever you want the linked table to be named
+            tbdef.Connect = "ODBC;Driver={Oracle in XE};dsn={Oracle - NXESL};dbq=XE;Uid=nxesl;Pwd=admin;Database=NXESL;Trusted_Connection=YES";
+            tbdef.SourceTableName = "NXESL.Article";//Whatever the SQL Server Table Name is
+            db.TableDefs.Append(tbdef);
         }
     }
 }
